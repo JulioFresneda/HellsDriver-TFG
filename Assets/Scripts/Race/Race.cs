7a224d8b6cfede7ec62ac37574c2dad55c2ff4e9
Julio A. Fresneda <juliofgx17@gmail.com>
2019-04-12 17:09:07 +0200

    start y finish

--- Assets/Scripts/Race/Race.cs
+++ Assets/Scripts/Race/Race.cs
@@ -3,60 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using VehicleSystem;
 
-public class Race : MonoBehaviour
+
+namespace Racing
 {
-    [SerializeField]
-    private int num_race_drivers = 10;
-
-    [SerializeField]
-    private GameObject carAI;
-
-    [SerializeField]
-    public GameObject carPlayer;
-
-
-    [SerializeField]
-    private Transform start;
-
-    private List<GameObject> race_drivers = new List<GameObject>();
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class Race : MonoBehaviour
     {
-      
-        
+        [SerializeField]
+        private int num_race_drivers = 10;
 
-        List<Vector3> start_positions = new List<Vector3>();
-        float x = start.position.x - 10;
-        float z = start.position.z - 10;
+        [SerializeField]
+        private GameObject carAI;
+
+        [SerializeField]
+        public GameObject carPlayer;
 
 
+        [SerializeField]
+        private Transform start;
 
-        for(int i=0; i<num_race_drivers; i++)
+        private List<RaceDriver> race_drivers = new List<RaceDriver>();
+
+        private static int position = 1;
+
+
+        // Start is called before the first frame update
+        void Start()
         {
-            start_positions.Add(new Vector3(x+ 10 * (i % 3), start.position.y, z+5*i));
-     
-            if(i<num_race_drivers-1) race_drivers.Add(Instantiate(carAI, start_positions[i],start.rotation));   
-            else race_drivers.Add(Instantiate(carPlayer, start_positions[i], start.rotation));
+            List<Vector3> start_positions = new List<Vector3>();
+            float x = start.position.x - 10;
+            float z = start.position.z - 10;
+
+
+
+            for (int i = 0; i < num_race_drivers; i++)
+            {
+                start_positions.Add(new Vector3(x + 10 * (i % 3), start.position.y, z + 5 * i));
+
+                if (i < num_race_drivers - 1)
+                {
+                    race_drivers.Add(Instantiate(carAI, start_positions[i], start.rotation).GetComponent<RaceDriver>());
+                }
+                else race_drivers.Add(Instantiate(carPlayer, start_positions[i], start.rotation).GetComponent<RaceDriver>());
+            }
+
+            race_drivers[num_race_drivers - 1].GetCar().GetComponent<CarController>().IsPlayer = true;
+            race_drivers[num_race_drivers - 1].GetCar().tag = "PlayerDriver";
+
+            foreach (RaceDriver g in race_drivers)
+            {
+                if (g.GetCar().tag != "PlayerDriver") g.GetCar().tag = "AIDriver";
+                g.GetCar().transform.Find("MinimapCar").transform.localScale = new Vector3(50f, 50f, 50f);
+            }
+
+            gameObject.GetComponent<Cameras>().StartCameras();
+
         }
 
-        race_drivers[num_race_drivers - 1].GetComponent<CarController>().IsPlayer = true;
-        race_drivers[num_race_drivers - 1].tag = "PlayerDriver";
-
-        foreach (GameObject g in race_drivers)
+        // Update is called once per frame
+        void Update()
         {
-            if (g.tag != "PlayerDriver") g.tag = "AIDriver";
-            g.transform.Find("MinimapCar").transform.localScale = new Vector3(50f, 50f, 50f);
+
         }
 
-        gameObject.GetComponent<Cameras>().StartCameras();
 
+
+
+
+        public static int GetPosition()
+        {
+            position++;
+            return position-1;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/Race/RaceDriver.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Racing
+{
+    public class RaceDriver : MonoBehaviour
+    {
+        
+
+        [SerializeField]
+        private GameObject car;
+
+        private string drivername;
+        private int position;
+        private float seconds;
+
+        private float startRaceTime;
+
+        private List<GameObject> checkpoints;
+        private List<GameObject> checkpoints_checked;
+
+
+        public void SetPosition(int p) => position = p;
+        public void SetSeconds(float t) => seconds = t;
+        public void SetName(string n) => drivername = n;
+
+        public GameObject GetCar() => car;
+
+        private void Start()
+        {
+            position = -1;
+            seconds = 0;
+            startRaceTime = Time.timeSinceLevelLoad;
+
+            checkpoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("CheckPoint"));
+            checkpoints_checked = new List<GameObject>();
+        }
+
+
+        private bool started = false;
+        void OnTriggerEnter(Collider col)
+        {
+            if (col.gameObject.name == "CheckStart")
+            {
+                if (started && checkpoints.Count == 0)
+                {
+                    position = Race.GetPosition();
+                    seconds = Time.timeSinceLevelLoad - startRaceTime;
+                    Debug.Log("Finish " + position + " " + seconds);
+                }
+                else
+                {
+                    Debug.Log("Started");
+                    started = true;
+                    if (!checkpoints_checked.Contains(col.gameObject))
+                    {
+                        checkpoints_checked.Add(col.gameObject);
+                    }
+                    if (checkpoints.Contains(col.gameObject)) checkpoints.Remove(col.gameObject);
+                }
+                
+            }
+            else if(col.gameObject.tag == "CheckPoint")
+            {
+                if (!checkpoints_checked.Contains(col.gameObject))
+                {
+                    checkpoints_checked.Add(col.gameObject);
+                }
+                if (checkpoints.Contains(col.gameObject)) checkpoints.Remove(col.gameObject);
+            }
+
+        }
+    }
+
+    
+
+}
