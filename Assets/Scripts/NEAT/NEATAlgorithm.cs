c395f6e06ea54b09c686d5abb5c20d7d8853e867
Julio A. Fresneda <juliofgx17@gmail.com>
2019-05-03 01:50:52 +0200

    e

--- Assets/Scripts/NEAT/NEATAlgorithm.cs
+++ Assets/Scripts/NEAT/NEATAlgorithm.cs
@@ -255,7 +255,7 @@ namespace NEAT
                     }
                 }
 
-                for(int i=0; i<12; i++)
+                for(int i=0; i<17; i++)
                 {
                     ChangeCar();
                 }
