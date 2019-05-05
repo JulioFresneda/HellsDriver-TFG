d11cdbfa1e10db8c53585236bc8e7e45d9e18059
Julio A. Fresneda <juliofgx17@gmail.com>
2019-05-05 21:37:26 +0200

    triangulos en minimapa

---
+++ Assets/ScreenData.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenData : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject CarMinimapIconPrefab;
+
+    [SerializeField]
+    private Material AICarMinimapIconMaterial;
+
+    [SerializeField]
+    private Material PlayerCarMinimapIconMaterial;
+
+
+    private List<GameObject> AICars;
+    private List<GameObject> AICarsIcons;
+
+    private GameObject PlayerCar;
+    private GameObject PlayerCarIcon;
+
+    // Start is called before the first frame update
+    void Initialize()
+    {
+        
+
+        AICars = new List<GameObject>();
+        AICars.AddRange(GameObject.FindGameObjectsWithTag("AIDriver"));
+
+        PlayerCar = GameObject.FindGameObjectWithTag("PlayerDriver");
+
+
+        AICarsIcons = new List<GameObject>();
+
+        for(int i=0; i<AICars.Count; i++)
+        {
+            AICarsIcons.Add(Instantiate(CarMinimapIconPrefab));
+            AICarsIcons[AICarsIcons.Count - 1].GetComponent<Renderer>().material = AICarMinimapIconMaterial;
+            AICarsIcons[AICarsIcons.Count - 1].GetComponent<Transform>().localScale = new Vector3(50, 50, 50);
+            AICarsIcons[AICarsIcons.Count - 1].layer = LayerMask.NameToLayer("Minimap");
+            AICarsIcons[AICarsIcons.Count - 1].transform.SetParent(AICars[AICarsIcons.Count - 1].transform);
+            AICarsIcons[AICarsIcons.Count - 1].transform.localPosition = new Vector3(0, 250, -20);
+        }
+        PlayerCarIcon = Instantiate(CarMinimapIconPrefab);
+        PlayerCarIcon.GetComponent<Renderer>().material = PlayerCarMinimapIconMaterial;
+        PlayerCarIcon.GetComponent<Renderer>().GetComponent<Transform>().localScale = new Vector3(50, 50, 50);
+        PlayerCarIcon.layer = LayerMask.NameToLayer("Minimap");
+
+        PlayerCarIcon.transform.SetParent(PlayerCar.transform);
+        PlayerCarIcon.transform.localPosition = new Vector3(0, 280, -20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerCar == null) Initialize();
+        else
+        {
+            
+            PlayerCarIcon.transform.rotation = PlayerCar.transform.rotation;
+
+            for (int i = 0; i < AICars.Count; i++)
+            {
+
+                AICarsIcons[i].transform.rotation = AICars[i].transform.rotation;
+ 
+            }
+        }
+
+        
+    }
+}
--- Assets/Scripts/Race/Cameras.cs
+++ Assets/Scripts/Race/Cameras.cs
@@ -6,24 +6,27 @@ public class Cameras : MonoBehaviour
 {
 
     private GameObject playercamera, minimap;
-    // Start is called before the first frame update
+
+
+    private GameObject player;
     public void StartCameras()
     {
         playercamera = GameObject.FindGameObjectWithTag("MainCamera");
         minimap = GameObject.Find("MinimapCamera");
 
-        playercamera.transform.SetParent(GameObject.FindGameObjectWithTag("PlayerDriver").transform);
+        player = GameObject.FindGameObjectWithTag("PlayerDriver");
+        playercamera.transform.SetParent(player.transform);
         playercamera.transform.localPosition = new Vector3(0, 3, -8);
         playercamera.transform.localRotation = new Quaternion(0, 0, 0, 0);
 
-        minimap.transform.SetParent(GameObject.FindGameObjectWithTag("PlayerDriver").transform);
-        minimap.transform.localPosition = new Vector3(0, 300, 0);
-        minimap.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);
+        minimap.transform.position = new Vector3(player.transform.position.x, 300, player.transform.position.z);
+        minimap.transform.rotation = Quaternion.Euler(90f, player.transform.eulerAngles.y, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        minimap.transform.position = new Vector3(player.transform.position.x, 300, player.transform.position.z);
+        minimap.transform.rotation = Quaternion.Euler(90f, player.transform.eulerAngles.y, 0);
     }
 }
