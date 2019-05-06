445214cd527efc775f51b0c9c16ed38d3bdadea9
Julio A. Fresneda <juliofgx17@gmail.com>
2019-05-06 18:48:42 +0200

    nitro bar

---
+++ Assets/Scripts/UI/RaceUI/SpeedometerUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VehicleSystem;
+
+public class SpeedometerUI : MonoBehaviour
+{
+
+    private double CurrentNitro;
+    private double MaxNitro;
+
+
+    private GameObject PlayerCar;
+
+    [SerializeField]
+    private Transform NitroBar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerCar == null) PlayerCar = GameObject.FindGameObjectWithTag("PlayerDriver");
+        else
+        {
+            CurrentNitro = PlayerCar.GetComponent<CarController>().Boost;
+            MaxNitro = PlayerCar.GetComponent<CarController>().MaxBoost;
+
+            NitroBar.GetComponent<Image>().fillAmount = (float)CurrentNitro / (float)MaxNitro;
+        }
+    }
+}
