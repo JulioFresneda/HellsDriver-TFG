d3bd00149232a57697ddefd809e7122f968bbbf6
Julio A. Fresneda <juliofgx17@gmail.com>
2019-05-18 00:57:59 +0200

    intro casi hecha, particulas

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+
+
+    
+
+
+
+
+
     public void CarreraRapida()
     {
         SceneManager.LoadScene("CarreraRapidaScene");
---
+++ Assets/Scripts/UI/MainMenuUI/ButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Texture TextureNotHighlighted;
+    public Texture TextureSelected;
+    public Texture TextureHighligted;
+   
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.GetComponent<RawImage>().texture = TextureHighligted;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        gameObject.GetComponent<RawImage>().texture = TextureSelected;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponent<RawImage>().texture = TextureNotHighlighted;
+    }
+}
