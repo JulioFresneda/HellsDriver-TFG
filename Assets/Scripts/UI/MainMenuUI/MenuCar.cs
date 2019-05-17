72d100de87fb85df6ff0eb99f737af05ec5ff1b0
Julio A. Fresneda <juliofgx17@gmail.com>
2019-05-17 16:56:17 +0200

    improve menu

---
+++ Assets/Scripts/UI/MainMenuUI/MenuCar.cs
@@ -0,0 +1,24 @@
+﻿using NeuralNet;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VehicleSystem;
+
+public class MenuCar : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject car = null;
+
+    [SerializeField]
+    string CarAIName = "car0__2000_3";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NNToFile ntf = new NNToFile();
+        car.GetComponent<CarAI>().SetNeuralNetwork(ntf.Read(CarAIName + ".txt"));
+    }
+
+}
