4549e746a6cd88bf61f0d07cd90ca2183771e6db
Julio A. Fresneda <juliofgx17@gmail.com>
2019-05-19 20:03:52 +0200

    marcas

---
+++ Assets/Scripts/UI/CR_SelectCarUI/SelectBrand.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectBrand : MonoBehaviour
+{
+
+    public GameObject Left, Center, Right;
+
+
+    private List<string> brands;
+
+    private int position;
+
+    [SerializeField]
+    private Transform transformSelected;
+
+    [SerializeField]
+    private Transform transformNotSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        brands = new List<string>();
+        brands.Add("Duck");
+        brands.Add("Audidas");
+        brands.Add("Hoa");
+        brands.Add("Valkyria");
+        brands.Add("XLynx");
+        brands.Add("DJED");
+        brands.Add("Raijin");
+        brands.Add("Poseidon");
+        brands.Add("Leviathan");
+
+        position = 0;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
