e8cfe3d11daffa90bbeaf15b30f416b1d7983349
Julio A. Fresneda <juliofgx17@gmail.com>
2019-05-23 04:10:29 +0200

    raijin

---
+++ Assets/Scripts/CarModels/RaijinRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VehicleSystem;
+
+public class RaijinRotation : MonoBehaviour
+{
+
+    public float constant = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0, 0, 1 * GetComponentInParent<CarController>().Speed * constant));
+    }
+}
