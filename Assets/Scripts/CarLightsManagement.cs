c45d3246d6b48c56dbeabdc97f77fa5e57a349b5
Julio A. Fresneda <juliofgx17@gmail.com>
2019-05-31 15:58:56 +0200

    subway

---
+++ Assets/Scripts/CarLightsManagement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarLightsManagement : MonoBehaviour
+{
+    public GameObject carLight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+        GameObject.FindGameObjectWithTag("PlayerDriver");
+
+
+        carLight.transform.SetParent(GameObject.FindGameObjectWithTag("PlayerDriver").transform);
+        carLight.transform.localPosition = Vector3.zero;
+        carLight.transform.localRotation = Quaternion.identity;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
